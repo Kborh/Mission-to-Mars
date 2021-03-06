0697974419712e835573b50a7039425473920a60
Karganleh  Borh <karganlehborh@Karganlehs-MacBook-Pro.local>
2021-03-06 12:21:31 -0600

    initial  commit

--- Mission_to_Mars.py
+++ Mission_to_Mars.py
@@ -5,68 +5,93 @@ from bs4 import BeautifulSoup as soup
 import pandas as pd
 
 # Path to chromedrive
-!which chromedriver
+#!which chromedriver
 # Mac users
 executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
 browser = Browser('chrome', **executable_path, headless=False)
 
 
-# Visit the mars nasa news site
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
+def mars_news(browser):
+    # Visit the mars nasa news site
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
 
-# Optional delay for loading the page
-browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    # Optional delay for loading the page
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
-# Convert the browser html to a soup object and then quit the browser
-# news=""
-html = browser.html
-news_soup = soup(html, 'html.parser')
+    # Convert the browser html to a soup object and then quit the browser
+    # news=""
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-slide_elem = news_soup.select_one('ul.item_list li.slide')
+  # Add try/except for error handling
+  try:
 
-# Assign the title and summery text to veriable
-slide_elem.find("div", class_='content_title')
+    slide_elem = news_soup.select_one('ul.item_list li.slide')
 
-# Use the parent element to find the first #'a'tag snd save it as
-# news_title
-news_title = slide_elem.find("div", class_='content_title').get_text()
-news_title
+    # Assign the title and summery text to veriable
+    slide_elem.find("div", class_='content_title')
 
-# Use the parent element to find the paragraph #text
-new_p = slide_elem.find('div', class_="article_teaser_body").get_text()
-new_p
+    # Use the parent element to find the first #'a'tag snd save it as
+    # news_title
+    news_title = slide_elem.find("div", class_='content_title').get_text()
+    news_title
 
-# JPL Special Featured Images Featuere Image
-# Visit URL
-url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
-browser.visit(url)
+    # Use the parent element to find the paragraph #text
+    new_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+    new_p
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+  except AttributeError:
+    return None, None
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    return news_title, new_p
 
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
+def featured_image(browser):
 
-# Use the base URL to create an absolute URL
-img_url = f'https// data-clsss-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
-img_url
+    # JPL Special Featured Images Featuere Image
+    # Visit URL
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url)
 
-df = pd.read_html('http://space-facts.com/mars/')[0]
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# Mars Facts
-df.columns = ['description', 'value']
-df.set_index('description', inplace=True)
-df
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+  # Add try/except for error handling
+  try:
+
+    # Find the relative image url
+    img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+    img_url_rel
+
+  except AttributeError:
+    return None
+    # Use the base URL to create an absolute URL
+    img_url = f'https// data-clsss-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+
+    return img_url
 
 
-df.to_html()
+def mars_facts():
+
+  try:
+    # Use 'read_html' to scrape the facts table into a dataframe
+    df = pd.read_html('http://space-facts.com/mars/')[0]
 
 
-browser.quit()
+  except BaseException:
+    return Non
+    # Mars Facts
+    df.columns = ['description', 'value']
+    df.set_index('description', inplace=True)
+    df
+
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html()
+
+
+    browser.quit()
